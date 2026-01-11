a61dac708181d6fcdaeb6f341a5a0db740cfb46f
Jeet Chauhan <spgtjeet123@gmail.com>
2026-01-11 13:50:24 +0530

    implemented hero section without ice falling cocktail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -22,6 +23,8 @@ function App() {
     // </>
     <main>
       <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
     </main>
   )
 }
