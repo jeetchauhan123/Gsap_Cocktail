06392264b30eae04e8f54c08782a542a1bce8648
Jeet Chauhan <spgtjeet123@gmail.com>
2026-01-13 11:29:10 +0530

    implemented about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import gsap from 'gsap'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -26,6 +27,7 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   )
 }
